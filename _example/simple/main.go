1496cf3aaa62864c95c405248f78d2d62fbb1c01
yterajima <terra@e2esound.com>
2015-10-04 15:37:53 +0900

    Add examples

---
+++ _example/simple/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"github.com/yterajima/go-sitemap"
+)
+
+func main() {
+	smap, err := sitemap.Get("http://www.e2esound.com/sitemap.xml")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Print URL in sitemap.xml
+	for _, URL := range smap.URL {
+		fmt.Println(URL.Loc)
+	}
+}
