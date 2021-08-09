460a9191a39887d0e8cfa1be122737de895037f2
Yuya Matsushima <yuyam@fillin-inc.com>
2021-08-09 14:17:37 +0900

    Add Example test
    replace `_example` dir

---
+++ sitemap_example_test.go
@@ -0,0 +1,60 @@
+package sitemap
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+func ExampleGet() {
+	smap, err := Get("https://issueoverflow.com/sitemap.xml", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, URL := range smap.URL {
+		fmt.Println(URL.Loc)
+	}
+}
+
+func ExampleGet_changeFetch() {
+	SetFetch(func(URL string, options interface{}) ([]byte, error) {
+		req, err := http.NewRequest("GET", URL, nil)
+		if err != nil {
+			return []byte{}, err
+		}
+
+		// Set User-Agent
+		req.Header.Set("User-Agent", "MyBot")
+
+		// Set timeout
+		timeout := time.Duration(10 * time.Second)
+		client := http.Client{
+			Timeout: timeout,
+		}
+
+		// Fetch data
+		res, err := client.Do(req)
+		if err != nil {
+			return []byte{}, err
+		}
+		defer res.Body.Close()
+
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			return []byte{}, err
+		}
+
+		return body, err
+	})
+
+	smap, err := Get("https://issueoverflow.com/sitemap.xml", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, URL := range smap.URL {
+		fmt.Println(URL.Loc)
+	}
+}
